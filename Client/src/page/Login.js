e3130f4d7a55b68f5891f0908b5252866df1ba7c
효승 <hyoseung@hyoseung-ui-MacBook-Pro.local>
2022-10-07 19:35:30 +0900

    페이지 생성

---
+++ Client/src/page/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>로그인페이지입니당!</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ Client/src/page/ProductDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return <div>상품디테일 페이지입니당 !</div>;
+};
+
+export default ProductDetail;
